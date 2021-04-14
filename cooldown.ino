62ce677d77769e0d910694b52a46a0515203579f
DanyCoolDev <66198246+DanyCoolDev@users.noreply.github.com>
2021-04-14 08:21:26 +0200

    Main File

---
+++ cooldown.ino
@@ -0,0 +1,54 @@
+
+#include <IRremote.h>
+
+int IRpin = 11;  // pin for the IR sensor
+
+int start = 0;
+bool istimerrunning; 
+
+void starttimer() {
+  start = millis();
+  istimerrunning = true; 
+}
+
+void stoptimer() {
+  istimerrunning = false; 
+}
+
+bool istimerout(unsigned long tm) {
+  
+  if(istimerrunning) {
+    unsigned long now = millis();
+    unsigned long ubehlo = now-start;
+    if(ubehlo > tm) {
+        stoptimer();
+        return true;
+    }
+    return false;
+  }
+  stoptimer();
+  return true;
+}
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  IrReceiver.begin(IRpin);  
+  stoptimer();               
+}
+
+void loop() {
+  bool timerout = istimerout(9000);
+  IrReceiver.begin(IRpin);
+  Serial.println(timerout);
+  if(timerout) {
+    if (IrReceiver.decode()) {
+      IrReceiver.resume(); 
+      digitalWrite(LED_BUILTIN, HIGH);  
+      delay(500);                       
+      digitalWrite(LED_BUILTIN, LOW);  
+      delay(500);   
+      starttimer();
+    }
+  }
+}
